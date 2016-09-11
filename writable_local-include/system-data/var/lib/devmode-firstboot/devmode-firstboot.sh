60f55589ce983aa9347d084c5abd35c70d5dcb48
Chen-Han Hsiao (Stanley) <stanley.hsiao@canonical.com>
2016-09-11 16:51:31 +0800

    fix bug

--- writable_local-include/system-data/var/lib/devmode-firstboot/devmode-firstboot.sh
+++ writable_local-include/system-data/var/lib/devmode-firstboot/devmode-firstboot.sh
@@ -6,8 +6,8 @@ set -e
 # import variable RECOVERYFSLABEL, RECOVERY_TYPE
 . /writable/system-data/var/lib/devmode-firstboot/conf.sh
 
-find /writable/system-data/var/lib/oem/snaps-devmode/ -name "*.snap" -type f -exec snap install --devmode {} +
-find /writable/system-data/var/lib/oem/snaps/ -name "*.snap" -type f -exec snap install {} +
+find /writable/system-data/var/lib/oem/snaps-devmode/ -name "*.snap" -type f -exec snap install --devmode {} \;
+find /writable/system-data/var/lib/oem/snaps/ -name "*.snap" -type f -exec snap install {} \;
 
 # run hooks
 hookdir=/writable/system-data/var/lib/devmode-firstboot/hooks.d/
